9b70213c2a43baad3adeef1c17d2998fc9eebea7
Illia <ilyachernykan@gmail.com>
2023-09-16 19:33:03 +0300

    Add LoaderUsers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import NavMenu from "./components/NavMenu";
+import Home from "./pages/Home";
+import UsersPage from "./pages/UsersPage";
 
-
+import Page404 from "./pages/Page404";
+import LoaderPage from "./pages/LoaderPage";
 function App() {
   return (
-    <>
-  <button onClick={()=>{window.open('https://randomuser.me/')}}>open</button>
-  <button onClick={()=>{window.close()}}>close</button>
-    </>
+    <BrowserRouter>
+      <NavMenu />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/users" element={<UsersPage />} />
+        <Route path="/loader" element={<LoaderPage />} />
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
